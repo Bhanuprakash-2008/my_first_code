33eea5ac396e56598ae48e6f49cd8d4a3d8b7e8d
Bhanuprakash c <bhanu352008@gmail.com>
2026-04-28 17:59:05 +0530

    Add basic arithmetic functions to calculator
    Implement basic arithmetic operations in a simple calculator.

---
+++ simple caluclator.py
@@ -0,0 +1,20 @@
+def add(a,b):
+  print(a+b) 
+def sub(a,b):
+ print(a-b)
+def mul(a,b):
+ print(a*b)
+def div(a,b):
+ print(a/b)
+def mod(a,b):
+  print(a%b)
+def flor(a,b):
+  print(a//b)
+n1=int(input("enter first element:"))
+n2=int(input("enter second element:"))
+add(n1,n2)
+sub(n1,n2)
+mul(n1,n2)
+div(n1,n2)
+mod(n1,n2)
+flor(n1,n2)
