ef778effb0bcc6c156d3789f93790f66834efd5c
Bhanuprakash c <bhanu352008@gmail.com>
2026-04-30 20:35:02 +0530

    Fix variable names and email generation logic

--- generating email.py
+++ generating email.py
@@ -1,5 +1,5 @@
-a=input("enter firest name:")
-b=input("enter last name:")
+first_name=input("enter firest name:")
+last_name=input("enter last name:")
 c=int(input("enter your favourite number:"))
 d=c+5
-print(a+b+d+"@gmail.com")
+print(first_name+last_name+d+"@gmail.com")
